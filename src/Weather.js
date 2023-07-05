cb37f10d264fd6bcea8e285ad777c1292637d134
saeedeh88 <125872352+saeedeh88@users.noreply.github.com>
2023-07-05 09:37:03 +0330

    added basic styling

--- src/Weather.js
+++ src/Weather.js
@@ -3,33 +3,53 @@ import "./Weather.css"
 export default function Weather(){
     return (
       <div className="Weather">
-      <form>
-        <div className="row">
-        <div className="col-8"><input type="search" placeholder="Enter a city.." className="form-control"/></div>
-      <div className="col-4"> <input type="submit" value="Search" className="btn btn-primary"/></div></div>
-      </form>
+        <form>
+          <div className="row">
+            <div className="col-8">
+              <input
+                type="search"
+                placeholder="Enter a city.."
+                className="form-control"
+                autoFocus="on"
+              />
+            </div>
+            <div className="col-4">
+              {" "}
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-primary w-100"
+              />
+            </div>
+          </div>
+        </form>
         <h1>New York</h1>
         <ul>
           <li>Wednesday 7:00</li>
           <li>Partly Cloudy</li>
         </ul>
-        <div className="row">
+        <div className="row mt-3">
           <div className="col-6">
-            <img
-              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-              alt="Partly-cloudy"
-            ></img>
-            6°C
+            <div className="clearfix">
+              <img
+                src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+                alt="Partly-cloudy"
+                className="float-left"
+              ></img>
+              <div className="float-left">
+                <span className="temperature float-left">6</span>{" "}
+                <span className="unit">°C</span>
+              </div>
+            </div>
           </div>
           <div className="col-6">
             <ul>
-                <li>Precipitation:15%</li>
-                <li>Humidity:20%</li>
-                <li>Wind:20 km/h</li>
+              <li>Precipitation:15%</li>
+              <li>Humidity:20%</li>
+              <li>Wind:20 km/h</li>
             </ul>
           </div>
         </div>
-    
       </div>
     );
 }
